8adccdd8b7d0b5799f9e8e99dd863b2766e0a4a8
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-02-28 16:20:13 -0600

    Add play a single round logic to script.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,62 @@
 function getComputerChoice() {
-    const randNum = Math.floor(Math.random() * 3);
-    let computerChoice = '';
-    
-    if(randNum == 0) {
-        computerChoice = 'rock';
-    } else if (randNum == 1) {
-        computerChoice = 'paper';
-    } else {
-        computerChoice = 'scissors';
-    }
+  const randNum = Math.floor(Math.random() * 3);
+  let computerChoice = "";
 
-    return computerChoice;
+  if (randNum == 0) {
+    computerChoice = "rock";
+  } else if (randNum == 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter rock, paper, or scissors.");
-    
-    return humanChoice;
+  let humanChoice = prompt("Enter rock, paper, or scissors.");
+
+  return humanChoice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  let winner = "";
+  if (humanChoice === computerChoice) {
+    //same answer
+    console.log("Same choice. No winner.");
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      humanScore++;
+      console.log("You win! Rock beats scissors!");
+    } else {
+      computerScore++;
+      console.log("You lose! Paper beats rock!");
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      humanScore++;
+      console.log("You win! Paper beats rock!");
+    } else {
+      computerScore++;
+      console.log("You lose! Scissors beat paper!");
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      humanScore++;
+      console.log("You win! Scissors beat paper!");
+    } else {
+      computerScore++;
+      console.log("You lose! Rock beats scissors!");
+    }
+  } else {
+    console.log("Invalid input");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
